51a4cf4c480bc9d87f2a46d4d5fee4ea8fe81668
Ashish Kumar Yadav <ashish11yadav01@gmail.com>
2026-08-05 23:20:46 +0530

    Added Termux widget files for Android

---
+++ termux/iiserlogin.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+username="iiser.login"
+password="wxyz1234"
+
+captiveportalsite="https://firewall.iiserpune.ac.in:8090"
+
+PRODUCTTYPE=0
+USERAGENT="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100 Safari/537.36"
+
+sendloginrequest() {
+    curl -k -s -m 3 -A "$USERAGENT" -X POST \
+        --url "$captiveportalsite/login.xml" \
+        --data-urlencode "mode=191" \
+        --data-urlencode "username=$username" \
+        --data-urlencode "password=$password" \
+        --data-urlencode "a=$(date +%s)000" \
+        --data-urlencode "producttype=$PRODUCTTYPE"
+}
+
+sendlogoutrequest() {
+    curl -k -s -m 3 -A "$USERAGENT" -X POST \
+        --url "$captiveportalsite/logout.xml" \
+        --data-urlencode "mode=193" \
+        --data-urlencode "username=$username" \
+        --data-urlencode "a=$(date +%s)000" \
+        --data-urlencode "producttype=$PRODUCTTYPE"
+}
+
+notconnectedexit() {
+    termux-notification -t "IISER Captive Portal" -c "Not connected to IISER network"
+    exit
+}
+
+loginfailed() {
+    termux-notification -t "IISER Captive Portal" -c "Could not log into IISER captive portal"
+}
+
+sendlogoutrequest >/dev/null 2>&1
+output="$(sendloginrequest)" || notconnectedexit
+printf '%s' "$output" | grep -qvFm1 "Login failed" || loginfailed
