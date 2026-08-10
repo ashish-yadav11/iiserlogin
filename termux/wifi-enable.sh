9d2cd0296d037929fed9ae250b18d29a025addfc
Ashish Kumar Yadav <ashish11yadav01@gmail.com>
2026-08-10 09:04:15 +0530

    More termux files

---
+++ termux/wifi-enable.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+connected() {
+    output="$(termux-wifi-connectioninfo)"
+    printf '%s' "$output" | grep -qF '"ssid": "Students",' &&
+        printf '%s' "$output" | grep -qF '"supplicant_state": "COMPLETED"'
+}
+
+termux-wifi-enable true
+t0="$(date +%s)"
+while ! connected ; do
+    sleep 1
+    [ "$(date +%s)" -gt "$((t0 + 10))" ] && exit
+done
+~/.shortcuts/tasks/iiserlogin.sh
+termux-job-scheduler --script ~/iiserlogin-job.sh --job-id 100 --period-ms 7200000
